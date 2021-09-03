082a9eb9a19fa8bfe8791faae3eb1b29142573cb
iKprkv <ikopryakov@hotmail.com>
2021-09-03 22:51:55 +0300

    scroll fix

--- html/beta/js/loader.js
+++ html/beta/js/loader.js
@@ -1,32 +1,36 @@
-//$(document).ready(function() {
-  var current_progress = 0,
-    step = 1; // the smaller this is the slower the progress bar
-  function startLoader(stop) {
-      interval = setInterval(function() {
-        current_progress += step;
-        progress = Math.round(Math.atan(current_progress) / (Math.PI / 2) * 100 * 1000) / 1000
-        $(".progress .num").text(Math.ceil(progress));
-        if (progress >= 100) {
-          clearInterval(interval);
-        } else if (progress >= 60) {
-          step = 0.4
-        }
-      }, 100);
-  }
-  startLoader(); // начинаем индикацию загрузки
+if ('scrollRestoration' in history) {
+  history.scrollRestoration = 'manual';
+}
+var current_progress = 0,
+  step = 1; // the smaller this is the slower the progress bar
+function startLoader(stop) {
+  interval = setInterval(function() {
+    current_progress += step;
+    progress = Math.round(Math.atan(current_progress) / (Math.PI / 2) * 100 * 1000) / 1000
+    $(".progress .num").text(Math.ceil(progress));
+    if (progress >= 100) {
+      clearInterval(interval);
+    } else if (progress >= 60) {
+      step = 0.4
+    }
+  }, 100);
+}
+startLoader(); // начинаем индикацию загрузки
 //});
-$(window).bind("load", function () {
-  setTimeout(function(){
+$(window).bind("load", function() {
+  window.scrollTo(0,0);
+  setTimeout(function() {
     $('.waiting-for-content').addClass('done');
   }, 500); //2000
-  setTimeout(function(){
+  setTimeout(function() {
     $("html, body").animate({
-        scrollTop: 0
-      }, 1
-    );
-    $('.waiting-for-content').fadeOut(function(){ $(this).remove() });
+      //scrollTop: 0
+    }, 0);
+    $('.waiting-for-content').fadeOut(function() {
+      $(this).remove()
+    });
     //$('.animationWait').each(function( index ) {
-      //$(this).removeClass('animationWait');
+    //$(this).removeClass('animationWait');
     //});
   }, 1000); //2500
 });
