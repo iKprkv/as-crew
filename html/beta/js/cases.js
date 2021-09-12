f1dd7d7ad84a2494c0954b36ff2d590b3cb954a2
iKprkv <ikopryakov@hotmail.com>
2021-09-12 22:39:22 +0300

    cases upd

--- html/beta/js/cases.js
+++ html/beta/js/cases.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
   var activeCase
   $("#cases .projects-list .case .text").on('mouseenter', function(e){
-    //activeCase = $(this).data('case');
+    //$('.teaser .content img').fadeOut( 'slow', function() {})
+    $('.teaser .content img').css('opacity','0')
     $("#cases .projects-list .case .text").removeClass('active');
     $(this).addClass('active');
+
     $('.teaser').removeClass('active');
-    $(this).siblings('.teaser').addClass('active');
-    //console.log('activeCase='+activeCase)
+    $(this).parent().siblings('.teaser').addClass('active');
+    //$('.teaser .content img').fadeIn( 'slow', function() {})
+    //$('.teaser .content img').show();
+    $('.teaser .content img').css('opacity','1')
   }).on('mouseleave', function(e){
 
   });
