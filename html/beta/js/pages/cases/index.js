82b43372c6b9ea8f89ae49183bcf8a748ef3fb1b
iKprkv <ikopryakov@hotmail.com>
2021-11-03 18:32:51 +0300

    cases js upd

--- html/beta/js/pages/cases/index.js
+++ html/beta/js/pages/cases/index.js
@@ -1,67 +1,16 @@
 if (window.innerWidth <= 900) {
   $('#cases').addClass('dark');
-  // var cursor = $('#cursor');
-  //
-  // $(document).ready(function() {
-  //
-  //   var clientX, clientY;
-  //
-  //   $('.cases-list').on('touchstart', function(e){
-  //     console.log('touch start');
-  //     clientX = e.touches[0].clientX;
-  //     clientY = e.touches[0].clientY;
-  //     //console.log(clientX+':'+clientY);
-  //
-  //     //$('.container.wide').scrollLeft(clientX)
-  //     //setTimeout(function(){
-  //       //cursor.css('left',e.touchItem.clientX);
-  //       //cursor.css('top',e.touchItem.clientY);
-  //       //console.log(e.touchItem.clientX+':'+e.touchItem.clientY)
-  //     //}, 20);
-  //   });
-  //   $('.cases-list').on('touchend', function(e){
-  //     console.log('touch end');
-  //     var deltaX, deltaY;
-  //       // Compute the change in X and Y coordinates.
-  //       // The first touch point in the changedTouches
-  //       // list is the touch point that was just removed from the surface.
-  //       deltaX = Math.abs(e.changedTouches[0].clientX - clientX);
-  //       deltaY = Math.abs(e.changedTouches[0].clientY - clientY);
-  //       console.log(deltaX+":"+deltaY)
-  //       $('.container.wide').animate({scrollLeft: clientX+deltaX,scrollTop: clientY+deltaY }, 800);
-  //
-  //       // Process the data ...
-  //   });
-  // //   $(document).on('touchmove', function(e){
-  // //     // Cache the client X/Y coordinates
-  // //     clientX = e.touches[0].clientX;
-  // //     clientY = e.touches[0].clientY;
-  // //   }, false);
-  // //
-  // // $(document).on('touchend', function(e){
-  // //   var deltaX, deltaY;
-  // //
-  // //   // Compute the change in X and Y coordinates.
-  // //   // The first touch point in the changedTouches
-  // //   // list is the touch point that was just removed from the surface.
-  // //   deltaX = e.changedTouches[0].clientX - clientX;
-  // //   deltaY = e.changedTouches[0].clientY - clientY;
-  // //
-  // //   // Process the data ...
-  // // }, false);
-  // });
-
 
   function centeringContent() {
     var outerContent = $('.container.wide');
     var innerContent = 0;
-    $(".cases-list .col").each(function(){
+    $(".cases-list .col").each(function() {
       //innerContent = innerContent+this.width();
       innerContent = innerContent + $(this).width()
     })
     console.log(innerContent);
     console.log(outerContent.width());
-    outerContent.scrollLeft( (innerContent - outerContent.width()) / 2);
+    outerContent.scrollLeft((innerContent - outerContent.width()) / 2);
   }
   $(document).ready(function() {
     $('#footer').remove();
@@ -81,80 +30,100 @@ if (window.innerWidth <= 900) {
 }
 $(document).ready(function() {
 
-  $('#cases .cases-list .case').mouseenter(function() {
-    $(this).find('svg').addClass('active').removeClass('fast');
-  });
-  $('#cases .cases-list .case').mouseleave(function() {
-    $(this).find('svg').removeClass('active').addClass('fast');
-  })
+  var casesData = [];
+  casesData['webdesign'] = [];
+  casesData['branding'] = [];
+  casesData['illustration'] = [];
+  casesData['appdesign'] = [];
+
+  var casesDataAll = $('#cases .cases-list').html();
+
+  function getCaseData() {
+    $($('#cases .cases-list .case')).each(function(index) {
+      if ($(this).data('type') == 'webdesign') {
+        casesData['webdesign'].push($(this).wrap('<div/>').parent().html());
+      } else if ($(this).data('type') == 'branding') {
+        casesData['branding'].push($(this).wrap('<div/>').parent().html())
+      } else if ($(this).data('type') == 'illustration') {
+        casesData['illustration'].push($(this).wrap('<div/>').parent().html())
+      } else if ($(this).data('type') == 'appdesign') {
+        casesData['appdesign'].push($(this).wrap('<div/>').parent().html())
+      }
+    });
+  }
+  getCaseData();
+  //console.log(casesData)
+  function caseAnimation() {
+    $('#cases .cases-list .case').mouseenter(function() {
+      $(this).find('svg').addClass('active').removeClass('fast');
+    });
+    $('#cases .cases-list .case').mouseleave(function() {
+      $(this).find('svg').removeClass('active').addClass('fast');
+    })
+  }
+
+  caseAnimation();
 
   if (window.innerWidth <= 900) {
-    $(".secondary-nav").on('click', function(){
+    $(".secondary-nav").on('click', function() {
       $(this).toggleClass('expanded')
     });
   }
-  $(".secondary-nav .link").on('click', function(){
-    if ($(this).hasClass('active')) {
-      //console.log('tap to active')
-    }
-    else {
+  $(".secondary-nav .link").on('click', function() {
+    if ($(this).hasClass('active')) {} else {
       $("#cases .cases-list").addClass('hold');
-    }
+
     $(this).addClass('active').siblings().removeClass('active');
+    $(".cases-list .col").html('');
+    $(".cases-list .col").css('transform', 'translate(0,0)');
     var type = $(this).data('type');
-    console.log(type);
-    $(".cases-list .col").removeClass('hidden');
+    //console.log(type);
     if (type == 'all') {
+
+      $("#cases .cases-list").html(casesDataAll)
+      caseAnimation();
       setTimeout(function() {
         $("#cases .cases-list").removeClass('hold')
-      },100)
-      $(".cases-list .case").each(function(){
-        $(this).show();
-      })
-    }
-    else {
-      //
-      setTimeout(function() {
-        $("#cases .cases-list").removeClass('hold')
-      },100)
-      $(".cases-list .case").each(function(){
-        if ($(this).data('type') == type) {
-          $(this).show();
+      }, 100)
+      // $(".cases-list .case").each(function() {
+      //   $(this).show();
+      // })
+    } else {
+      //console.log(casesData[type])
+      var i = 1;
+      $.each(casesData[type], function(key, value) {
+        //console.log(key)
+        if (i > 3) {
+          i = 1;
         }
-        else {
-          $(this).hide();
-        }
+        $(".cases-list .col:nth-child(" + i + ")").append(value)
+        i++
       });
-      $(".cases-list .col").each(function(){
+      caseAnimation();
+      setTimeout(function() {
+        $("#cases .cases-list").removeClass('hold');
 
-        //console.log($(this).children(':visible').length);
-
-        if($(this).children(':visible').length == 0) {
-          //$(this).addClass('hidden');
-
-        }
-      })
-      //
+      }, 100)
+    }
     }
     if (window.innerWidth < 900) {
       centeringContent();
     }
   });
   if (window.innerWidth >= 900) {
-  $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    //console.log(position)
-    $(".cases-list .col").each(function() {
-      //if ($(this).data('r_num')) {
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      //console.log(position)
+      $(".cases-list .col").each(function() {
+        //if ($(this).data('r_num')) {
         var translate = $(this).css('transform');
         var matrix = translate.replace(/[^0-9\-.,]/g, '').split(',');
         var y = parseInt(matrix[13]) || parseInt(matrix[5]);
         if (scroll > position) {
           if ($(window).scrollTop() <= 0) {
             $(this).css('transform', 'translate(0, 0)');
-          }
-          else {
-          //console.log('scroll down')
+          } else {
+            //console.log('scroll down')
             var num = y - ($(this).data('r_num'));
             $(this).css('transform', 'translate(0, ' + num + 'px)');
           }
@@ -169,8 +138,8 @@ $(document).ready(function() {
             $(this).css('transform', 'translate(0, ' + num + 'px)');
           }
         }
+      })
+      position = scroll;
     })
-    position = scroll;
-  })
   }
 })
