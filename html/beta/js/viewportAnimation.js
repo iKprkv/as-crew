0c90f0cec891d90fe708cab9ac3dccf13a911138
iKprkv <ikopryakov@hotmail.com>
2021-11-11 14:57:44 +0300

    notice and fixes

--- html/beta/js/viewportAnimation.js
+++ html/beta/js/viewportAnimation.js
@@ -1,24 +1,28 @@
-$(document).ready(function() {
-  $('.animationWait').each(function(index) {
-    //console.log($(this).attr('src'))
-    if ($(this).attr('src')) {
-      $(this).addClass('lazy');
-      //var src = $(this+'.lazy').attr('src')
-      //$('img.lazy').attr('lazy-src', src);
-    }
-    else {
-      $(this).find('img').addClass('lazy');
-      //var src = $(this).find('.lazy').attr('src')
-      //$('img.lazy').attr('lazy-src', src);
-    }
-    //$('img.lazy').after('<div class="load-elm-overlay"></div>');
-    //var src = $('img.lazy').attr('scr');
-    //console.log(src);
-
-    //$('img.lazy').removeAttr('src');
-  })
-
-})
+// $(document).ready(function() {
+//   $('.animationWait').each(function(index) {
+//     //console.log($(this).attr('src'))
+//     if ($(this).attr('src')) {
+//       $(this).addClass('lazy');
+//       var src = $(this).attr('src');
+//       console.log(src)
+//       $(this).attr('lazy-src', src).removeAttr('src');
+//
+//       //$('img.lazy').attr('lazy-src', src);
+//     }
+//     else {
+//       $(this).find('img').addClass('lazy');
+//       var src = $(this).find('.lazy').attr('src');
+//       console.log(src)
+//       //$('img.lazy').attr('lazy-src', src);
+//     }
+//     //$('img.lazy').after('<div class="load-elm-overlay"></div>');
+//     //var src = $('img.lazy').attr('scr');
+//     //console.log(src);
+//
+//     //$('img.lazy').removeAttr('src');
+//   })
+//
+// })
 $(window).bind("load", function () {
   var elements;
   var windowHeight;
@@ -38,6 +42,13 @@ $(window).bind("load", function () {
       if (h <= -vh) {
            $(element).removeClass('animationWait').addClass('animationDelay');
            //console.log(element)
+           if ($(element).attr('lazy-src')) {
+             var lSrc = $(element).attr('lazy-src');
+             $(element).attr('src', lSrc).removeAttr('lazy-src').removeClass('lazy');
+             $(element).one("load", function() {
+                $(this).css('background','red')
+             })
+           }
         init();
       }
 
@@ -52,6 +63,13 @@ $(window).bind("load", function () {
     if($(window).scrollTop() + ($(window).height() + 70) >= $(document).height()) {
       $(".animationWait").each(function() {
         $(this).removeClass('animationWait').addClass('animationDelay');
+        if ($(this).attr('lazy-src')) {
+          var lSrc = $(this).attr('lazy-src');
+          $(this).attr('src', lSrc).removeAttr('lazy-src').removeClass('lazy');
+          $(this).one("load", function() {
+             $(this).css('background','red')
+          })
+        }
       });
     }
     else {
